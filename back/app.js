a0984bbcbf276211f568a18b8e1b333e952ec023
kevin <khersinpro@gmail.com>
2023-05-23 20:55:24 +0200

    adding jwt guard and extractUserFromToken

--- back/app.js
+++ back/app.js
@@ -1,11 +1,12 @@
-const express       = require('express');
-const createError   = require('http-errors');
-const cookieParser  = require('cookie-parser');
-const logger        = require('morgan');
-const path          = require('path');
-const app           = express();
-const router        = require('./routes');
-const sequelize     = require('./Sequelize/database/sequelize');
+const express                     = require('express');
+const createError                 = require('http-errors');
+const cookieParser                = require('cookie-parser');
+const logger                      = require('morgan');
+const path                        = require('path');
+const app                         = express();
+const router                      = require('./routes');
+const sequelize                   = require('./Sequelize/database/sequelize');
+const { extractUserFromToken }    = require('./middleware/auth/auth');
 
 // Setting view engines
 app.set('views', path.join(__dirname, 'views'));
@@ -19,6 +20,7 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 //Router
+app.use(extractUserFromToken);
 app.use('/api', router);
 
 // catch 404 and forward to error handler
